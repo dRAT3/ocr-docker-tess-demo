7c26826b84a48110b280b5b160f41937715c0bfb
drat3 <drat3@protonmail.ch>
2024-05-29 15:49:47 +0200

    test

--- services/api/src/app/main.py
+++ services/api/src/app/main.py
@@ -2,6 +2,7 @@ from fastapi import FastAPI, Request
 from contextlib import asynccontextmanager
 from src.app.generic_exception_handler import generic_exception_handler
 from src.app.logger import setup_logging
+from src.ocr.minimum_viable_ocr import minimum_viable_ocr
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -17,5 +18,9 @@ app = FastAPI(lifespan=lifespan)
 async def ping():
     return "ponk"
 
+@app.get("/mvo")
+async def mvo():
+    return minimum_viable_ocr()
+    
 # Used for logging all uncatched exceptions
 app.add_exception_handler(Exception, generic_exception_handler)
