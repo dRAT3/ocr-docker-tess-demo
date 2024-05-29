d96d7a64fce44c51457be7a79c2d61a8d493df87
drat3 <drat3@protonmail.ch>
2024-05-29 15:33:19 +0200

    Added logs volume

--- services/api/run.py
+++ services/api/run.py
@@ -3,5 +3,4 @@ from src.app.main import app
 import logging
 
 if __name__ == "__main__":
-    logging.getLogger(__name__).info("Application started")
     uvicorn.run(app, host="0.0.0.0", port=8000)
