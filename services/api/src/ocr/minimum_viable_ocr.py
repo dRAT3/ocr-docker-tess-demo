9711df0caa5e3def53f76be475fe539746300e0b
drat3 <drat3@protonmail.ch>
2024-05-29 16:05:59 +0200

    Change path

--- services/api/src/ocr/minimum_viable_ocr.py
+++ services/api/src/ocr/minimum_viable_ocr.py
@@ -2,7 +2,7 @@ from pikepdf import Pdf
 import ocrmypdf
 
 def minimum_viable_ocr() -> str:
-    ocr = ocrmypdf.ocr("/app/test_data/sample.pdf", "/app/test_data/out.pdf", language='eng',rotate_pages=True, deskew=True, force_ocr=True, jobs=2)
+    ocr = ocrmypdf.ocr("/app/test_data/Schoolkidz-December-2021-statement.pdf", "/app/test_data/out.pdf", language='eng',rotate_pages=True, deskew=True, force_ocr=True, jobs=2)
     return ocr
 
 
