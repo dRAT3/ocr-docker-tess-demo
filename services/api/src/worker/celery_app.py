d374a4409515ecb1f42649598cc348515515f54c
drat3 <drat3@protonmail.ch>
2024-05-30 16:28:54 +0200

    os

--- services/api/src/worker/celery_app.py
+++ services/api/src/worker/celery_app.py
@@ -1,13 +1,14 @@
 from celery import Celery
 from src.logging.logger import setup_logging
 import logging
+import os
 
 from pikepdf import Pdf
 import ocrmypdf
 
 def make_celery(app_name=__name__):
     logger = logging.getLogger(__name__)
-    logger.info("Booting celery worker")
+    logger.info("Booting celery worker from {os.getpid()}")
     return Celery(app_name, broker="redis://redis:6379/0", backend="redis://redis:6379/0")
 
 setup_logging("celery")
