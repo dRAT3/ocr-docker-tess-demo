fdc304974c2116e2936460d7a5b6d6f64063d38c
drat3 <drat3@protonmail.ch>
2024-05-30 17:19:13 +0200

    file out []

--- services/api/src/app/main.py
+++ services/api/src/app/main.py
@@ -28,7 +28,7 @@ async def ping():
 @app.get("/mvo")
 async def mvo():
     file_out = f"{uuid.uuid4()}.pdf"
-    task = celery_app.send_task('ocr.minimum_viable_ocr', file_out)
+    task = celery_app.send_task('ocr.minimum_viable_ocr', args=[file_out])
     return {"file_out": file_out, "task_id": task.id}
 
 # Used for logging all uncatched exceptions
