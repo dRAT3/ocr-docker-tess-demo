05ccbd3221c528dc283418cb437c21d159ce5065
drat3 <drat3@protonmail.ch>
2024-05-30 15:38:43 +0200

    Logger redundancy

--- services/api/src/worker/celery_app.py
+++ services/api/src/worker/celery_app.py
@@ -1,7 +1,10 @@
 from celery import Celery
 from src.logging.logger import setup_logging
+import logging
 
 def make_celery(app_name=__name__):
+    logger = logging.getLogger(__name__)
+    logger.info("Booting celery worker")
     return Celery(app_name, broker="redis://redis:6379/0", backend="redis://redis:6379/0")
 
 setup_logging("celery")
