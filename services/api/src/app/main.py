6950372246594447abce62ab1adb987da3e3e5ad
drat3 <drat3@protonmail.ch>
2024-05-30 17:10:12 +0200

    try 2 restructure

--- services/api/src/app/main.py
+++ services/api/src/app/main.py
@@ -3,6 +3,8 @@ from contextlib import asynccontextmanager
 from src.app.generic_exception_handler import generic_exception_handler
 from src.logging.logger import setup_logging
 from src.ocr.minimum_viable_ocr import minimum_viable_ocr
+from celery import Celery
+import uuid
 
 @asynccontextmanager
 async def lifespan(app: FastAPI):
@@ -14,14 +16,21 @@ async def lifespan(app: FastAPI):
 
 app = FastAPI(lifespan=lifespan)
 
+celery_app = Celery(
+    'ocr',
+    broker='redis://redis:6379/0',
+    backend='redis://redis:6379/0'
+)
+
 @app.get("/ping")
 async def ping():
     return "ponk"
 
 @app.get("/mvo")
 async def mvo():
-    ret = await minimum_viable_ocr()
-    return ret
+    file_out = f"{uuid.uuid4()}.pdf"
+    task = celery_app.send_task('ocr.minimum_viable_ocr', file_out)
+    return {"file_out": file_out, "task_id": task.id}
 
 # Used for logging all uncatched exceptions
 app.add_exception_handler(Exception, generic_exception_handler)
--- services/worker/src/celery_app.py
+++ services/worker/src/celery_app.py
@@ -6,15 +6,15 @@ import os
 from pikepdf import Pdf
 import ocrmypdf
 
-def make_celery(app_name=__name__):
+def make_celery(app_name):
     logger = logging.getLogger(__name__)
     logger.info(f"Booting celery worker from {os.getpid()}")
     return Celery(app_name, broker="redis://redis:6379/0", backend="redis://redis:6379/0")
 
 setup_logging("celery")
-celery = make_celery()
+celery_app = make_celery("ocr")
 
-@celery.task
+@celery_app.task(name="ocr.minimum_viable_ocr")
 def minimum_viable_ocr_task(file_out: str):
     logger = logging.getLogger(__name__)
     logger.info("Starting minimum viable ocr task")
