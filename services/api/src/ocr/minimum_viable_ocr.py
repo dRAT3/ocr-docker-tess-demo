5f9c0ac28cdef25bbe69f16feeef4f417b881483
drat3 <drat3@protonmail.ch>
2024-05-30 16:07:55 +0200

    trying shit

--- services/api/src/ocr/minimum_viable_ocr.py
+++ services/api/src/ocr/minimum_viable_ocr.py
@@ -2,16 +2,9 @@ from pikepdf import Pdf
 import ocrmypdf
 import uuid
 import logging
-from src.worker.celery_app import celery
+from src.worker.celery_app import minimum_viable_ocr_task 
 
 async def minimum_viable_ocr():
     file_out = f"{uuid.uuid4()}.pdf"
     task = minimum_viable_ocr_task.delay(file_out)
     return {"file_out": file_out, "task_id": task.id}
-
-@celery.task
-def minimum_viable_ocr_task(file_out: str):
-    logger = logging.getLogger(__name__)
-    logger.info("Starting minimum viable ocr task")
-    ocr = ocrmypdf.ocr("/home/app/test_data/Schoolkidz-December-2021-statement.pdf", f"/home/app/test_data/{file_out}", language='eng',rotate_pages=True, deskew=True, force_ocr=True, jobs=2)
-    logger.info("Completed minimum viable ocr task")
--- services/api/src/worker/celery_app.py
+++ services/api/src/worker/celery_app.py
@@ -2,6 +2,7 @@ from celery import Celery
 from src.logging.logger import setup_logging
 import logging
 
+
 def make_celery(app_name=__name__):
     logger = logging.getLogger(__name__)
     logger.info("Booting celery worker")
@@ -9,3 +10,11 @@ def make_celery(app_name=__name__):
 
 setup_logging("celery")
 celery = make_celery()
+
+@celery.task
+def minimum_viable_ocr_task(file_out: str):
+    logger = logging.getLogger(__name__)
+    logger.info("Starting minimum viable ocr task")
+    ocr = ocrmypdf.ocr("/home/app/test_data/Schoolkidz-December-2021-statement.pdf", f"/home/app/test_data/{file_out}", language='eng',rotate_pages=True, deskew=True, force_ocr=True, jobs=2)
+    logger.info("Completed minimum viable ocr task")
+
